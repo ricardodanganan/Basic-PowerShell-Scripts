e0f1d7a6f8fee312f413fc810acb6c93805d60a9
Ricardo Danganan Jnr <83029812+ricardodanganan@users.noreply.github.com>
2024-04-24 12:20:32 +0100

    Add files via upload

---
+++ getDriveSpaceLog.ps1
@@ -0,0 +1,9 @@
+﻿# Description: This script will get the disk space of the drives and log it to a text file along with the current date and time.
+# Author: Ricardo Danganan
+
+# Get the current date and time using the Get-Date cmdlet and pipe it to the Out-File cmdlet to append it to the log file called "Disk-Space-log.txt".
+# Out-file is used to write the output of a command to a file.
+Get-Date | Out-File -FilePath C:\Users\RicardoDanganan\Desktop\Disk-Space-log.txt -Append
+# Get the disk space of all drives using the Get-PSDrive cmdlet with the -PSProvider parameter set to FileSystem.
+# Pipe the output to the Out-File cmdlet to append it to the log file.
+Get-PSDrive -PSProvider FileSystem | Out-file -FilePath C:\Users\RicardoDanganan\Desktop\Disk-Space-log.txt -Append
